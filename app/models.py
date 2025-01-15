8196abb55e146a617c71e9bf3633120aaeba0d08
Lioncookie2 <sebastian@fjellstua.net>
2025-01-15 12:36:38 +0100

    Add created_at field to Delay model

--- app/models.py
+++ app/models.py
@@ -3,16 +3,14 @@ from datetime import datetime
 
 class Delay(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    line = db.Column(db.String(20), nullable=False)
-    line_name = db.Column(db.String(100))
-    transport_type = db.Column(db.String(20), nullable=False)
+    line = db.Column(db.String(50), nullable=False)
+    station = db.Column(db.String(100), nullable=False)
     delay_minutes = db.Column(db.Integer, nullable=False)
-    station = db.Column(db.String(100))
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    journey_reference = db.Column(db.String(100), unique=True)  # Unikt ID for hver reise
-    
+    transport_type = db.Column(db.String(20), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
     def __repr__(self):
-        return f'<Delay {self.line} {self.delay_minutes}min>'
+        return f'<Delay {self.line} at {self.station}: {self.delay_minutes} min>'
 
 class DailySummary(db.Model):
     id = db.Column(db.Integer, primary_key=True)
